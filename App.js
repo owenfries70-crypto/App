9717f03b300f147fce624259bf37713d178380b2
owenfries70-crypto <owenfries70@gmail.com>
2026-06-05 15:22:59 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,22 @@
 function showAnswer() {
     document.getElementById("answer").textContent = "Hello";
 }
+const lesson = [
+  {
+    question: 'What does "Hallo" mean?',
+    choices: ["Goodbye", "Hello", "Thank you", "Please"],
+    answer: "Hello"
+  },
+
+  {
+    question: 'What does "Danke" mean?',
+    choices: ["Sorry", "Thank you", "Morning", "Friend"],
+    answer: "Thank you"
+  },
+
+  {
+    question: 'What does "Ja" mean?',
+    choices: ["No", "Yes", "Maybe", "Good"],
+    answer: "Yes"
+  }
+];
